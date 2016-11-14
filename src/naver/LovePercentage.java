528077c0834c62a5a17505fa6ec93185307580be
premiumMina <premiumMina@gmail.com>
2016-11-14 20:51:09 +0900

    [#12] 이름으로 궁합 알아보기 예) 이화신, 표나리 이 표 화 나 신 리 2  7  7  3  4  4  9  4  0  7  8   3  4  7  5    7  1  2     8  3 83% 의 궁합을 가진다. 단, 문자의 길이가 다를 경우, 찰리브라운, 표나리 찰 표 리 나 브 리 라 운 의 순서로 계산을 진행한다.

---
+++ src/naver/LovePercentage.java
@@ -0,0 +1,76 @@
+package naver;
+
+public class LovePercentage {
+	public int len(char word) {
+		int val = 0;
+		switch (word) {
+		case '이':
+			val = 2;
+			break;
+		case '화':
+			val = 7;
+			break;
+		case '신':
+			val = 4;
+			break;
+		case '표':
+			val = 7;
+			break;
+		case '나':
+			val = 3;
+			break;
+		case '리':
+			val = 4;
+			break;
+		}
+		return val;
+	}
+
+	public void calcMC(String A, String B) {
+		/* 문자 A를 무조건 긴 문자로 정하기 */
+		if (A.length() < B.length()) {
+			String tmp = A;
+			A = B;
+			B = tmp;
+		}
+
+		/* 이표화나신리 완료 */
+		String newName = "";
+		for (int idx = 0; idx < A.length(); idx++) {
+			if (idx < B.length()) {
+				newName = newName + A.substring(idx, idx + 1);
+				newName = newName + B.substring(idx, idx + 1);
+			} else {
+				newName = newName + A.substring(idx, idx + 1);
+			}
+		}
+
+		/* 각 이름의 값을 가져와서 배열에 넣는다. */
+		int newSize = newName.length();
+		int[] result = new int[newSize];
+		for (int idx = 0; idx < newSize; idx++) {
+			result[idx] = len(newName.charAt(idx));
+		}
+
+		
+		/*
+		 * 글자길이의 -2 한 만큼 계산이 반복되는 것을 알았다. 
+		 * 총 길이가 7이라면 5번의 계산을 통해서 궁합을 구할 수 있다.
+		 */
+		int count = 0;
+		while (newSize - count != 2) {
+			for (int i = 0; i < newSize - 1; i++) {
+				result[i] = (result[i] + result[i + 1]) % 10;
+			}
+			count++;
+		}
+
+		int percentage = result[0] * 10 + result[1];
+		System.out.println(percentage+"%");
+
+	}
+
+	public static void main(String[] args) {
+		new LovePercentage().calcMC("이화신", "표나리");
+	}
+}

d88d1814ff1a2be3c8cf5e6731e814aa650e172d
premiumMina <premiumMina@gmail.com>
2017-01-27 16:53:40 +0900

    [#33] 타일 채우기

---
+++ src/acmicpc/Q2193.java
@@ -0,0 +1,19 @@
+package acmicpc;
+
+import java.util.Scanner;
+
+public class Q2193 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] dp = new int[n + 1];
+		dp[0] = 1;
+		for (int i = 2; i <= n; i += 2) {
+			dp[i] = dp[i - 2] * 3;
+			for (int j = i - 4; j >= 0; j -= 2) {
+				dp[i] += dp[j] * 2;
+			}
+		}
+		System.out.println(dp[n]);
+	}
+}

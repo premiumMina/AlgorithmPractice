59db3f492d058a6959ce355116c9949ce9e2af97
premiumMina <premiumMina@gmail.com>
2017-01-27 16:55:12 +0900

    [#33] 타일 채우기

--- src/acmicpc/Q2133.java
+++ src/acmicpc/Q2133.java
@@ -2,7 +2,7 @@ package acmicpc;
 
 import java.util.Scanner;
 
-public class Q2193 {
+public class Q2133 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();

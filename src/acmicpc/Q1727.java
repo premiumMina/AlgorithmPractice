71d95cec681d2a9e0e7f4a1b92becacf2824464d
premiumMina <premiumMina@gmail.com>
2017-02-06 16:30:04 +0900

    [#36] 커플 만들기 주석

--- src/acmicpc/Q1727.java
+++ src/acmicpc/Q1727.java
@@ -23,6 +23,7 @@ public class Q1727 {
 		Arrays.sort(narr);
 		Arrays.sort(marr);
 
+		// dp[i][j]가 의미하는 것: man[i]와 woman[i]가 커플이 된 경우
 		int[][] dp = new int[n + 1][m + 1];
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= m; j++) {
@@ -33,9 +34,11 @@ public class Q1727 {
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= m; j++) {
 				dp[i][j] = Math.min(dp[i][j], dp[i - 1][j - 1] + Math.abs(narr[i] - marr[j]));
+				// i가 큰경우 여성 j와 매치된 모든 남자와의 최소 값을 비교하여 제일 작은 값을 구한다.
 				if (i > j) {
 					dp[i][j] = Math.min(dp[i][j], dp[i - 1][j]);
 				}
+				// j가 큰경우 i와 매치되는 여성 j를 모두 비교하여 제일 작은 값을 구한다.
 				if (i < j) {
 					dp[i][j] = Math.min(dp[i][j], dp[i][j - 1]);
 				}
